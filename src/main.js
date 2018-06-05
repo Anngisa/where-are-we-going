eda255a8d14472f88faeb6adb5658fde2186b7c0
Anngisa <1060341755@qq.com>
2018-06-05 22:04:47 +0800

    change

--- src/main.js
+++ src/main.js
@@ -5,12 +5,17 @@ import axios from 'axios'
 import App from './App'
 import router from './router'
 import fastClick from 'fastclick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+
 import 'styles/reset.css'
 import 'styles/border.css'
 import 'styles/iconfont.css'
+import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
+
 Vue.prototype.$axios=axios
 
 /* eslint-disable no-new */
---
+++ src/pages/home/Components/Swiper.vue
@@ -0,0 +1,40 @@
+<template>
+	<div class="wrapper">
+		 <swiper :options="swiperOption">
+	    <swiper-slide v-for="item of swiperOption.swiperList" :key=item.id>
+	    	<img class="swiper-img" :src="item.imgUrl">
+	    </swiper-slide>
+	    <div class="swiper-pagination"  slot="pagination"></div>
+	  </swiper>
+  </div>
+</template>
+<script>
+export default{
+	name:'HomeSwiper',
+ 	data() {
+    return {
+      swiperOption: {
+        pagination: {
+          el: '.swiper-pagination',
+        },
+        loop:true,
+        swiperList:[
+        	{id:'0001',imgUrl:'http://img1.qunarzz.com/piao/fusion/1609/f8/ef0b29100632f002.jpg_750x200_ebe20c8f.jpg'},
+        	{id:'002',imgUrl:'http://img1.qunarzz.com/piao/fusion/1806/8b/2680212fadf30002.jpg_750x200_f85409ac.jpg'}]
+      }
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+	.wrapper >>>.swiper-pagination-bullet-active
+		background:#fff !important
+	.wrapper
+		width:100%
+		height:0
+		overflow:hidden
+		background:#fff
+		padding-bottom:26.66%
+		.swiper-img
+			width:100%
+</style>
