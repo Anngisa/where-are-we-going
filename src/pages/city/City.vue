1420e32cb0a8c1f53eaebd5a0cf21b74fb3c5e9e
Anngisa <1060341755@qq.com>
2018-06-11 17:20:44 +0800

    city-router

---
+++ src/pages/city/City.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>
+		<city-header></city-header>
+	</div>
+</template>
+<script>
+import CityHeader from './Components/Header.vue'
+export default{
+	name:'City',
+	components:{
+		CityHeader
+	}
+}
+</script>
+<style lang="stylus">
+	
+</style>
