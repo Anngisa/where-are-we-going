23cd8ca9a88b073ff5521029cfb77df9d91975bc
Anngisa <1060341755@qq.com>
2018-07-01 16:25:24 +0800

    detail-list

--- src/pages/detail/Components/Header.vue
+++ src/pages/detail/Components/Header.vue
@@ -63,6 +63,7 @@ export default{
 		top:0
 		left:0
 		right:0 
+		z-index:2
 		height:.86rem
 		line-height:.86rem
 		text-align:center
--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -3,18 +3,41 @@
 		<detail-banner></detail-banner>
 		<detail-header></detail-header>
 		<div class="content">
-			
+			<detail-list :list="list"></detail-list>
 		</div>
 	</div>	
 </template>
 <script>
 	import detailBanner from './Components/Banner'; 
 	import detailHeader from './Components/Header'; 
+	import detailList from './Components/List'
 	export default{
 		name:'Detail',
 		components:{
 			detailBanner,
-			detailHeader
+			detailHeader,
+			detailList
+		},
+		data(){
+			return {
+				list:[{
+					title:'成人票',
+					children:[{
+						title:'成人三馆联票',
+						children:[{
+							title:'成人三馆联票-连锁店'
+						}]
+					},{
+						title:'成人五馆联票'
+					}]
+				},{
+					title:'学生票'
+				},{
+					title:'儿童票'
+				},{
+					title:'特惠票'
+				}]
+			}
 		}
 	}
 </script>
